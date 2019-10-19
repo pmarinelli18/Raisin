43fe3a516facce2fd75a4a8f7b0dcf075886426e
Julian Berrio <ja.berrio@ufl.edu>
2019-10-19 18:53:39 -0400

    Updates

--- Raisin/Form1.cs
+++ Raisin/Form1.cs
@@ -58,7 +58,6 @@ namespace Raisin
 
         private void enter_Click(object sender, EventArgs e)
         {
-            //ResultsLogic r = new ResultsLogic(txtInput.Text);
             int count = 0;
         if (youtubeURL.Text != "")
             {
@@ -81,6 +80,7 @@ namespace Raisin
                 errorMessage.Text = "Error: You must provide input";
             }
 
+            ResultsLogic r = new ResultsLogic(txtInput.Text);
         }
     }
 }
