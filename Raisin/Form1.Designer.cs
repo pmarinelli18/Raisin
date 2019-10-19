4c5972bfd77d8b00254be3eb7bf492aab96da5bc
pmarinelli18 <pmarinelli@ufl.edu>
2019-10-19 03:23:49 -0400

    Began the form

--- Raisin/Form1.Designer.cs
+++ Raisin/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.TextBox();
+            this.searchFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.enter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(490, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input a paragraph or use look up a file";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(119, 198);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(676, 348);
+            this.input.TabIndex = 1;
+            this.input.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // searchFile
+            // 
+            this.searchFile.Location = new System.Drawing.Point(746, 106);
+            this.searchFile.Name = "searchFile";
+            this.searchFile.Size = new System.Drawing.Size(276, 57);
+            this.searchFile.TabIndex = 2;
+            this.searchFile.Text = "Search File";
+            this.searchFile.UseVisualStyleBackColor = true;
+            this.searchFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 597);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 32);
+            this.label2.TabIndex = 3;
+            // 
+            // enter
+            // 
+            this.enter.Location = new System.Drawing.Point(134, 618);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(221, 40);
+            this.enter.TabIndex = 4;
+            this.enter.Text = "Enter";
+            this.enter.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1360, 843);
+            this.Controls.Add(this.enter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.searchFile);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button searchFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button enter;
     }
 }
 
