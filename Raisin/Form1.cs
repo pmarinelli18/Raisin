5b9b5a36649de089ca869edc5e89398837d29e96
pmarinelli18 <pmarinelli@ufl.edu>
2019-10-19 03:47:42 -0400

    Added File finding

--- Raisin/Form1.cs
+++ Raisin/Form1.cs
@@ -34,7 +34,16 @@ namespace Raisin
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            System.Windows.Forms.FolderBrowserDialog dlg = new System.Windows.Forms.FolderBrowserDialog();
+            // This is what will execute if the user selects a folder and hits OK (File if you change to FileBrowserDialog)
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string folder = dlg.SelectedPath;
+            }
+            else
+            {
+                // This prevents a crash when you close out of the window with nothing
+            }
         }
     }
 }
