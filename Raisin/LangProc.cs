074ab6f418f466fa22466b1cec5aac3f60d9baa7
Julian Berrio <ja.berrio@ufl.edu>
2019-10-19 04:56:00 -0400

    Added Cloud Lanuge Api

---
+++ Raisin/LangProc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Language.V1;
+
+namespace Raisin
+{
+    class LangProc
+    {
+        public LangProc()
+            {
+
+            }
+
+
+    }
+}
